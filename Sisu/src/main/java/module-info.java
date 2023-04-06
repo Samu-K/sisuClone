9eb47077025a88a052132b229daf439617adab8c
Eemil Heikkilä <eemil.heikkila@tuni.fi>
2023-04-06 17:09:20 +0300

    Updated module-info to include subfolders api and backend, gui still missing?

--- Sisu/src/main/java/module-info.java
+++ Sisu/src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module fi.tuni.prog3.sisu {
     requires javafx.controls;
     requires transitive javafx.graphics;
-    exports fi.tuni.prog3.sisu;
+    exports fi.tuni.prog3.sisu.backend;
+    exports fi.tuni.prog3.sisu.api;
     requires transitive com.google.gson;
 }
