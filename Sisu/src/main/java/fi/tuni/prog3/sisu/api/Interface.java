76b984ae835abf916c1bc2b8c3d02260ad3bb958
Eemil Heikkilä <eemil.heikkila@tuni.fi>
2023-04-07 00:05:20 +0300

    made small modifications to hopefully make the program build

--- Sisu/src/main/java/fi/tuni/prog3/sisu/api/Interface.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/api/Interface.java
@@ -1,9 +1,13 @@
 package fi.tuni.prog3.sisu.api;
 
+import java.util.ArrayList;
+
 /**
  * Public interface to access information from the Sisu API from the rest of the program.
  */
 
 public class Interface {
-    
+  public ArrayList<String> getDegreeProgrammeNames() {
+    return DegreeProgrammeGetter.getDegreeProgramNamesFromApi("https://sis-tuni.funidata.fi/kori/api/module-search?curriculumPeriodId=uta-lvv-2021&universityId=tuni-university-root-id&moduleType=DegreeProgramme&limit=1000");
+  }
 }
