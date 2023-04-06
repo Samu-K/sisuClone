27d3d33230b2d4ca20e02ffc39b0e7c4956d6462
Samu-K <samu.kaarlela@gmail.com>
2023-04-06 13:57:57 +0300

    Changed location for files
    Added main in resources
    Made template for studies screen

--- Sisu/src/main/java/fi/tuni/prog3/sisu/backend/Sisu.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/backend/Sisu.java
@@ -1,21 +1,30 @@
 package fi.tuni.prog3.sisu.backend;
 
+import java.net.URL;
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-
 /**
- * JavaFX Sisu.
+ * Main sisu class.
  */
 public class Sisu extends Application {
+  public static void main(String[] args) {
+    launch(args);
+  }
 
   @Override
-  public void start(Stage stage) {
-      
-  }
+  public void start(Stage primaryStage) throws Exception {
+    FXMLLoader loader = new FXMLLoader();
+    URL url = new URL("file:src/main/java/fi/tuni/prog3/sisu/gui/studies.fxml");
+    loader.setLocation(url);
+    VBox vbox = loader.<VBox>load();
 
-  public static void main(String[] args) {
-    launch();
+    Scene scene = new Scene(vbox);
+    primaryStage.setScene(scene);
+    primaryStage.show();
   }
-   
+>>>>>>> 13251da (Changed location for files)
 }
--- Sisu/src/main/java/module-info.java
+++ Sisu/src/main/java/module-info.java
@@ -1,7 +1,13 @@
 module fi.tuni.prog3.sisu {
     requires javafx.controls;
     requires transitive javafx.graphics;
+
     exports fi.tuni.prog3.sisu.backend;
     exports fi.tuni.prog3.sisu.api;
+
+    requires transitive javafx.fxml;
+
     requires transitive com.google.gson;
+
+
 }
