515bec545581e8919b0a2f173c7153123bbeee20
Samu-K <samu.kaarlela@gmail.com>
2023-04-10 17:38:25 +0300

    Finished studies screen base
    It now has the search combo box and the sidebar
    controller not made

--- Sisu/src/main/java/fi/tuni/prog3/sisu/backend/Sisu.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/backend/Sisu.java
@@ -1,11 +1,18 @@
 package fi.tuni.prog3.sisu.backend;
 
+import fi.tuni.prog3.sisu.backend.AutoCompleteComboBox.HideableItem;
 import java.net.URL;
+import java.util.Arrays;
+import java.util.List;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import javafx.util.StringConverter;
 
 /**
  * Main sisu class.
@@ -20,12 +27,46 @@ public class Sisu extends Application {
     FXMLLoader loader = new FXMLLoader();
     URL url = new URL("file:src/main/java/fi/tuni/prog3/sisu/gui/studies.fxml");
     loader.setLocation(url);
-    BorderPane vbox = loader.<BorderPane>load();
-    Scene scene = new Scene(vbox);
+    BorderPane main = loader.<BorderPane>load(); 
+    setupCombo(main);
+
+    Scene scene = new Scene(main);
 
     primaryStage.setScene(scene);
     primaryStage.setTitle("SIM");
     primaryStage.show();
   }
 
+  private static void setupCombo(BorderPane main) {
+    ComboBox<HideableItem<String>> dropDown;
+
+    String[] programmes = {"1", "12", "132"};
+    List<String> locales = Arrays.asList(programmes);
+
+    dropDown = AutoCompleteComboBox.createComboBoxWithAutoCompletionSupport(
+        locales, 
+        new StringConverter<HideableItem<String>>() {           
+        @Override
+        public String toString(HideableItem<String> object) {
+          if (object != null) {                    
+            return object.getObject();
+          } else {
+            return null;
+          }
+        }
+
+        @Override
+        public HideableItem<String> fromString(String string) {
+          String foundString = locales.stream().filter((String i)
+                          -> (i).equals(string)).findFirst().orElse(null);
+          return new HideableItem<>(foundString, this);
+
+        }
+        
+      });
+      
+    main.setCenter(dropDown);
+    main.setAlignment(dropDown, Pos.TOP_LEFT);
+    main.setMargin(dropDown, new Insets(12, 12, 12, 12));
+  }
 }
--- Sisu/src/main/java/module-info.java
+++ Sisu/src/main/java/module-info.java
@@ -1,12 +1,10 @@
 module fi.tuni.prog3.sisu {
     requires javafx.controls;
     requires transitive javafx.graphics;
+    requires transitive javafx.fxml;
+    requires transitive com.google.gson;
+    requires transitive org.apache.commons.lang3;
 
     exports fi.tuni.prog3.sisu.backend;
     exports fi.tuni.prog3.sisu.api;
-
-    requires transitive javafx.fxml;
-
-    requires transitive com.google.gson;
-
 }
