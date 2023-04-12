abe787ede5b885f8046a468472d8da46f245ee93
Samu-K <samu.kaarlela@gmail.com>
2023-04-13 00:00:47 +0300

    Improved controller
    Also made sure Sisu.java is not doing controller work
    
    Next step is to populate

--- Sisu/src/main/java/fi/tuni/prog3/sisu/backend/Sisu.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/backend/Sisu.java
@@ -1,19 +1,11 @@
 package fi.tuni.prog3.sisu.backend;
 
-import fi.tuni.prog3.sisu.backend.AutoCompleteComboBox.HideableItem;
 import java.net.URL;
-import java.util.ArrayList;
-import java.util.Arrays;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
 import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
-import javafx.util.StringConverter;
 
 /**
  * Main sisu class.
@@ -30,18 +22,6 @@ public class Sisu extends Application {
     loader.setLocation(stdUrl);
 
     BorderPane main = loader.<BorderPane>load();
-
-    /*
-    // get the container setup in fxml
-    HBox degreeCont = (HBox) main.lookup("#degreeContainer");
-    // create the actual dropdown menu
-    ComboBox<HideableItem<String>> degreeDropDown = setupCombo(
-        new ArrayList<String>(Arrays.asList("JOKU KIVA OPINTO", "LISÄÄ OPINTOJA", "car3"))
-    );
-    degreeDropDown.setPromptText("Degree programme...");
-    // add that menu to our container
-    degreeCont.getChildren().add(degreeDropDown);
-    */
     
     Scene scene = new Scene(main);
     primaryStage.setScene(scene);
@@ -49,33 +29,4 @@ public class Sisu extends Application {
     primaryStage.show();
   }
 
-  private static ComboBox<HideableItem<String>> setupCombo(ArrayList<String> items) {
-    ComboBox<HideableItem<String>> dropDown;
-
-    dropDown = AutoCompleteComboBox.createComboBoxWithAutoCompletionSupport(
-        items, 
-        new StringConverter<HideableItem<String>>() {           
-        @Override
-        public String toString(HideableItem<String> object) {
-          if (object != null) {                    
-            return object.getObject();
-          } else {
-            return null;
-          }
-        }
-
-        @Override
-        public HideableItem<String> fromString(String string) {
-          String foundString = items.stream().filter((String i)
-                          -> (i).equals(string)).findFirst().orElse(null);
-          return new HideableItem<>(foundString, this);
-
-        }
-        
-      }
-    );
-    dropDown.getStylesheets().add("file:src/resources/css/dropdown.css");
-    
-    return dropDown;
   }
-}
--- Sisu/src/main/java/fi/tuni/prog3/sisu/backend/StudiesController.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/backend/StudiesController.java
@@ -1,9 +1,17 @@
 package fi.tuni.prog3.sisu.backend;
 
 import javafx.fxml.FXML;
+import javafx.geometry.Pos;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
 import javafx.scene.layout.StackPane;
+import javafx.util.StringConverter;
+import javafx.scene.layout.HBox;
+import javafx.scene.control.ComboBox;
+import java.util.Arrays;
+import java.util.ArrayList;
+import fi.tuni.prog3.sisu.backend.AutoCompleteComboBox.HideableItem;
+import javafx.scene.layout.VBox;
 
 /**
  * Controller class for studies.fxml.
@@ -12,6 +20,10 @@ public class StudiesController {
 
   @FXML
   private StackPane mainPane;
+  @FXML
+  private HBox degreeCont;
+  @FXML
+  private VBox courseBox;
 
   @FXML
   private void initialize() {
@@ -21,8 +33,46 @@ public class StudiesController {
         new TreeItem<String>("Item 2")
     );
     TreeView<String> courseView = new TreeView<String>(root);
-    mainPane.getChildren().add(courseView);
+    courseBox.getChildren().add(courseView);
 
-    System.out.println("Init ready");
+    // create the actual dropdown menu
+    ComboBox<HideableItem<String>> degreeDropDown = setupCombo(
+        new ArrayList<String>(Arrays.asList("JOKU KIVA OPINTO", "LISÄÄ OPINTOJA", "car3"))
+    );
+    degreeDropDown.setPromptText("Degree programme...");
+    // add that menu to our container
+    degreeCont.getChildren().add(degreeDropDown);
+    
   }
+
+  private static ComboBox<HideableItem<String>> setupCombo(ArrayList<String> items) {
+    ComboBox<HideableItem<String>> dropDown;
+
+    dropDown = AutoCompleteComboBox.createComboBoxWithAutoCompletionSupport(
+        items, 
+        new StringConverter<HideableItem<String>>() {           
+        @Override
+        public String toString(HideableItem<String> object) {
+          if (object != null) {                    
+            return object.getObject();
+          } else {
+            return null;
+          }
+        }
+
+        @Override
+        public HideableItem<String> fromString(String string) {
+          String foundString = items.stream().filter((String i)
+                          -> (i).equals(string)).findFirst().orElse(null);
+          return new HideableItem<>(foundString, this);
+
+        }
+        
+      }
+    );
+    dropDown.getStylesheets().add("file:src/resources/css/dropdown.css");
+    
+    return dropDown;
+  }
+
 }
