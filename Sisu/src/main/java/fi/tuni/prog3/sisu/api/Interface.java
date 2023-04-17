095644724cbe3c98d5a8fafbe64e6e02977ebe30
Eemil Heikkilä <eemil.heikkila@tuni.fi>
2023-04-17 23:28:15 +0300

    StudyModule class implemented. NOT TESTED

--- Sisu/src/main/java/fi/tuni/prog3/sisu/api/Interface.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/api/Interface.java
@@ -1,5 +1,6 @@
 package fi.tuni.prog3.sisu.api;
 
+import com.google.gson.JsonObject;
 import java.util.ArrayList;
 
 /**
@@ -9,5 +10,28 @@ import java.util.ArrayList;
 public class Interface {
   public ArrayList<String> getDegreeProgrammeNames() {
     return DegreeProgrammeGetter.getDegreeProgramNamesFromApi("https://sis-tuni.funidata.fi/kori/api/module-search?curriculumPeriodId=uta-lvv-2021&universityId=tuni-university-root-id&moduleType=DegreeProgramme&limit=1000");
+  }
+
+  /**
+   * Returns a JsonObject that contains information about a degree programme. The program calls the
+   * right API based on whether the id represents a course or a study module.
+
+   * @param id id or groupId of the module being retrieved. 
+   * @return JsonObject that contains the module information.
+   */
+  public static JsonObject getDegreeModuleById(String id) {
+    String url = "";
+
+    if (id.startsWith("otm")) {
+      // The ID represents a module
+      url = "https://sis-tuni.funidata.fi/kori/api/modules/" + id; 
+    } else {
+      // The ID represents a course
+      url = "https://sis-tuni.funidata.fi/kori/api/course-units/by-group-id?groupId=" + id + "&universityId=tuni-university-root-id";
+    }
+
+    JsonObject jsonObject = ApiJsonGetter.getJsonFromApi(url);
+
+    return jsonObject;
   }
 }
--- Sisu/src/main/java/fi/tuni/prog3/sisu/backend/StudyModule.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/backend/StudyModule.java
@@ -1,5 +1,7 @@
 package fi.tuni.prog3.sisu.backend;
 
+import com.google.gson.JsonObject;
+import fi.tuni.prog3.sisu.api.Interface;
 import java.util.ArrayList;
 
 /**
@@ -8,14 +10,109 @@ import java.util.ArrayList;
  */
 public class StudyModule extends DegreeModule {
 
-  private ArrayList<DegreeModule> coursesAndSubModules;
+  private ArrayList<DegreeModule> childModules;
+  private boolean isMandatory;
+  private ArrayList<String> childGroupIds;
+  private boolean childrenHaveBeenAdded = false;
 
-  public StudyModule(String name, String id, String groupId, int minCredits) {
+  private void getChildrenFromApi() {
+    if (!childrenHaveBeenAdded) {
+      for (String childGroupId : childGroupIds) {
+        JsonObject childJsonObject = Interface.getDegreeModuleById(childGroupId);
+        // TODO: Build the object in ObjectBuilders class
+        // Create a mock object for now
+        DegreeModule childModule = new Course("Johdatus todennäköisyyslaskentaan", "MATH.APP.210", "tut-xxx-xxx-xxx", 5, true, false); // = ObjectBuilders.buildDegreeModule(childJsonObject);
+        childModules.add(childModule);
+      }
+      childrenHaveBeenAdded = true;
+    }
+  }
+
+  /**
+   * A constructor for initializing the member variables.
+
+   * @param name name of the StudyModule.
+   * @param id id of the StudyModule.
+   * @param groupId group id of the StudyModule.
+   * @param minCredits minimum credits of the StudyModule.
+   * @param isMandatory whether the StudyModule is mandatory or not.
+   * @param childGroupIds group ids of the children of this StudyModule.
+   */
+  public StudyModule(String name, String id, String groupId, int minCredits, boolean isMandatory,
+      ArrayList<String> childGroupIds) {
     super(name, id, groupId, minCredits);
-    //TODO Auto-generated constructor stub
+    
+    this.isMandatory = isMandatory;
+    this.childGroupIds = childGroupIds;
   }
   
-  public ArrayList<DegreeModule> getCoursesAndSubModules() {
-    return coursesAndSubModules;
+  /**
+   * Returns courses under this StudyModule.
+
+   * @return ArrayList of courses under this StudyModule.
+   */
+  public ArrayList<Course> getCourses() {
+    ArrayList<Course> courses = new ArrayList<Course>();
+    for (DegreeModule dm : childModules) {
+      if (dm instanceof Course) {
+        courses.add((Course) dm);
+      }
+    }
+    return courses;
+  }
+
+  /**
+   * Returns StudyModules under this StudyModule.
+
+   * @return ArrayList of StudyModules under this StudyModule.
+   */
+  public ArrayList<StudyModule> getSubModules() {
+    ArrayList<StudyModule> subModules = new ArrayList<StudyModule>();
+    for (DegreeModule dm : childModules) {
+      if (dm instanceof StudyModule) {
+        subModules.add((StudyModule) dm);
+      }
+    }
+    return subModules;
+  }
+
+  public boolean isMandatory() {
+    return isMandatory;
+  }
+
+  /**
+   * Returns all children of this StudyModule recursively. If StudyModule is mandatory, all children
+   * of its children are also returned.
+
+   * @return ArrayList of all DegreeModules under this StudyModule.
+   */
+  public ArrayList<DegreeModule> getChildrenRecursively() {
+    if (!childrenHaveBeenAdded) {
+      getChildrenFromApi();
+    }
+
+    ArrayList<DegreeModule> children = new ArrayList<DegreeModule>();
+    for (DegreeModule dm : childModules) {
+      children.add(dm);
+      if (dm instanceof StudyModule && ((StudyModule) dm).isMandatory) {
+        children.addAll(((StudyModule) dm).getChildrenRecursively());
+      }
+    }
+    return children;
+  }
+
+  /**
+   * Returns children of this Studymodule. Courses are returned first, then StudyModules.
+
+   * @return ArrayList of children of this StudyModule.
+   */
+  public ArrayList<DegreeModule> getChildren() {
+    if (!childrenHaveBeenAdded) {
+      getChildrenFromApi();
+    }
+    ArrayList<DegreeModule> children = new ArrayList<DegreeModule>();
+    children.addAll(getCourses());
+    children.addAll(getSubModules());
+    return children;
   }
 }
