6118c572f76931d8f91fc42e61be2aea08cff8b9
Samu-K <samu.kaarlela@gmail.com>
2023-04-24 22:00:46 +0300

    Added in documentation

--- Sisu/src/main/java/fi/tuni/prog3/sisu/backend/StudiesController.java
+++ Sisu/src/main/java/fi/tuni/prog3/sisu/backend/StudiesController.java
@@ -4,20 +4,20 @@ import fi.tuni.prog3.sisu.api.Interface;
 import fi.tuni.prog3.sisu.backend.AutoCompleteComboBox.HideableItem;
 import java.util.ArrayList;
 import java.util.TreeMap;
+import javafx.application.Platform;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
 import javafx.scene.control.TreeItem;
 import javafx.scene.control.TreeView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.util.StringConverter;
 
-import javafx.event.EventHandler;
-import javafx.scene.input.MouseEvent;
-import javafx.scene.control.ListView;
-import javafx.application.Platform;
-import javafx.scene.control.Label;
 
 /**
  * Controller class for studies.fxml.
@@ -45,6 +45,8 @@ public class StudiesController {
     // add that menu to our container
     degreeCont.getChildren().add(degreeDropDown);
     degreeDropDown.setPromptText("Search for Degree Programmes...");
+
+    // listens for a value selection on the combobox
     degreeDropDown.sceneProperty().addListener((a, oldScene, newScene) -> {
       if (newScene == null || cboxMouseEventHandler != null) {
         return;
@@ -54,14 +56,17 @@ public class StudiesController {
       if (listView != null) {
         cboxMouseEventHandler = (e) -> {
           Platform.runLater(() -> {
+            // find the chose degree
             HideableItem<String> selectedValue = (HideableItem<String>) listView.getSelectionModel().getSelectedItem();
+            // create our course tree based on degree (tba)
             TreeView<String> courseView = createCourseTree();
+            // remove the combobox and swap in label to show selection
             courseBox.getChildren().clear();
             courseBox.getChildren().add(courseView);
             Label degree = new Label(selectedValue.toString());
             degree.setStyle("-fx-font-size:18;"
-              +"-fx-text-fill:white;"
-              +"-fx-padding: 25 0 0 25;");
+                    + "-fx-text-fill:white;"
+                    + "-fx-padding: 25 0 0 25;");
             courseBox.getChildren().add(0, degree);
 
 
@@ -74,6 +79,9 @@ public class StudiesController {
 
     
   }
+  /** 
+   * shorthand to make handling combobox mouse events easier.
+   */
 
   private EventHandler<MouseEvent> cboxMouseEventHandler;
   
@@ -109,7 +117,7 @@ public class StudiesController {
         
       }
     );
-    dropDown.getStylesheets().add("file:src/resources/css/dropdown.css");
+  dropDown.getStylesheets().add("file:src/resources/css/dropdown.css");
     
     return dropDown;
   }
@@ -145,6 +153,11 @@ public class StudiesController {
     return groupId;
   }
 
+  /**
+   * Create the treeview that containes all courses for chose degree.
+   *
+   * @return a filled out and styled treeview
+   */
   private static TreeView<String> createCourseTree() {
     TreeItem<String> root1 = new TreeItem<String>("Yhteiset opinnot");
     root1.getChildren().addAll(
@@ -167,8 +180,6 @@ public class StudiesController {
     return courseView;
   }
 
-  private void setDegree() {
-  }
 }
 
 
